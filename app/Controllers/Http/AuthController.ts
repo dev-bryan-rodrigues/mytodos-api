189b0d92c56de67da935eb24a3908b2acf4a8b6b
Bryan Rodrigues <bryan.rodrigues@braip.com>
2023-03-06 14:37:38 -0300

    return user in token response

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,14 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class AuthController {
   public async store({ request, auth }: HttpContextContract) {
     const { name, password } = request.all()
-
+    const user = await User.query().where('name', name)
     const token = await auth.attempt(name, password, {
-      expiresIn: '30 days'
+      expiresIn: '1 day'
     })
 
-    return token
+    return { token, user }
   }
 
   public async destroy({ auth }: HttpContextContract) {
